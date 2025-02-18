ba8c415b5691e63f296256f4a5ee6f792ff0942d
huynhtrong123 <duytronghuynh@gmail.com>
2025-02-18 10:47:30 +0700

    Create startreservation_01_insert table transation.sql

---
+++ startreservation_01_insert table transation.sql
@@ -0,0 +1,38 @@
+declare @lotNotHold int = 0;
+if ('{{Workflow.Properties.blLotNotHold}}' = 'true')
+begin
+set @lotNotHold = 1;
+end
+
+--declare @recipeText nvarchar(100) = concat('{{Workflow.Properties.portID}}',"    /" , '{{Workflow.Properties.physicalRecipe}}')
+declare @recipeText nvarchar(100) ='{{Workflow.Properties.physicalRecipeID}}';
+
+UPDATE t set [blLotNotHold] = @lotNotHold
+FROM [SIVIEW_INTEGRATION].[dbo].[TxStartLotsReservationReq] t where ID = {{Workflow.Properties.ReservationReqID}}
+
+INSERT INTO [SIVIEW_INTEGRATION].[dbo].[MACHINE_TRANSACTION]
+           ([MACHINE_ID]
+           ,[PORT_ID]
+           ,[LOT_ID]
+           ,[TRANSACTION_STATUS_ID]
+           ,[REQUEST_DATA]
+           ,[RESPOND_DATA]
+           ,[DESCRIPTION]
+           ,[METHOD_ID]
+           ,[TIMESTAMP]
+           ,[EQUIPMENT_ID]
+           ,[CASSETTE_ID], [RECIPE_ID], [IS_PROCESSED], [TxStartLotsReservationReqID])
+     VALUES
+           ('{{Workflow.Properties.machineID}}'
+           ,'{{Workflow.Properties.portID}}'
+           ,'{{Workflow.Properties.lotID}}'
+           ,1
+           ,''
+           ,''
+           ,''
+           ,0
+           ,getdate()
+           ,'{{Workflow.Properties.equipmentID}}'
+           ,'{{Workflow.Properties.cassetteID}}', @recipeText, 0, {{Workflow.Properties.ReservationReqID}})
+		   
+		   select @@Identity
